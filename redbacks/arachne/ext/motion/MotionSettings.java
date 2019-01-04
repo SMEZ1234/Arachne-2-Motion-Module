a813b5cacb319bc1e8b473715f12cf3e6a06ba11
SMEZ1234 <smez12345@gmail.com>
2019-01-04 23:43:53 +1100

    Added foot and metre getters and setters for encoder ticks per metre

--- redbacks/arachne/ext/motion/MotionSettings.java
+++ redbacks/arachne/ext/motion/MotionSettings.java
@@ -9,8 +9,45 @@ package redbacks.arachne.ext.motion;
  */
 public class MotionSettings
 {
-	/** The change in encoder tick value when the robot travels precisely 1 metre. Sorry, imperial system users - you'll have to do the conversion. */
-	public static int encoderTicksPerMetre; // FIXME Hide variable and create getters and setters for imperial and metric systems.
+	private static double encoderTicksPerMetre;
+	private static final double METRES_PER_FOOT = 0.3048;
+	
+	/**
+	 * Sets a global constant for the change in encoder tick value when the robot travels precisely 1 metre.
+	 * 
+	 * @param ticks The number of ticks.
+	 */
+	public static void setEncoderTicksPerMetre(int ticks) {
+		encoderTicksPerMetre = ticks;
+	}
+	
+	/**
+	 * Sets a global constant for the change in encoder tick value when the robot travels precisely 1 foot.
+	 * The value is stored as a number of encoder ticks per metre.
+	 * 
+	 * @param ticks The number of ticks.
+	 */
+	public static void setEncoderTicksPerFoot(int ticks) {
+		encoderTicksPerMetre = ticks / METRES_PER_FOOT;
+	}
+	
+	/**
+	 * Gets the value of a constant set by {@link #setEncoderTicksPerMetre(int)} or {@link #setEncoderTicksPerFoot(int)}.
+	 * 
+	 * @return The change in encoder tick value when the robot travels precisely 1 metre.
+	 */
+	public static int getEncoderTicksPerMetre() {
+		return (int) encoderTicksPerMetre;
+	}
+	
+	/**
+	 * Gets the value of a constant set by {@link #setEncoderTicksPerMetre(int)} or {@link #setEncoderTicksPerFoot(int)}.
+	 * 
+	 * @return The change in encoder tick value when the robot travels precisely 1 foot.
+	 */
+	public static int getEncoderTicksPerFoot() {
+		return (int) (encoderTicksPerMetre * METRES_PER_FOOT);
+	}
 	
 	/**
 	 * The number of yet-to-be-reached trajectory waypoints that are taken and averaged to determine the desired angle for the robot.
--- redbacks/arachne/ext/motion/trajectories/Path.java
+++ redbacks/arachne/ext/motion/trajectories/Path.java
@@ -10,7 +10,7 @@ public class Path
 	
 	public Path(double[]... waypoints) {
 		this.waypoints = waypoints;
-		totalDistance = waypoints[waypoints.length-1][0] * MotionSettings.encoderTicksPerMetre;
+		totalDistance = waypoints[waypoints.length-1][0] * MotionSettings.getEncoderTicksPerMetre();
 	}
 	
 	public void reset() {
@@ -18,24 +18,18 @@ public class Path
 	}
 	
 	public double getAngleFromDistance(double distance) {
-		while(progressIndex < waypoints.length - 1 && Math.abs(distance) > Math.abs(waypoints[progressIndex][0] * MotionSettings.encoderTicksPerMetre)) {
-			progressIndex++;
-		}
+		while(progressIndex < waypoints.length - 1 && Math.abs(distance) > Math.abs(waypoints[progressIndex][0] * MotionSettings.getEncoderTicksPerMetre())) progressIndex++;
 		
 		double[] angles = new double[Math.min(MotionSettings.trajectoryAngleForesight, waypoints.length - progressIndex)];
-		
 		for(int i = 0; i < angles.length; i++) angles[i] = waypoints[i + progressIndex][1];
 		
 		return avg(angles);
 	}
 	
 	public double getCurvatureFromDistance(double distance) {
-		while(progressIndex < waypoints.length - 1 && Math.abs(distance) > Math.abs(waypoints[progressIndex][0] * MotionSettings.encoderTicksPerMetre)) {
-			progressIndex++;
-		}
+		while(progressIndex < waypoints.length - 1 && Math.abs(distance) > Math.abs(waypoints[progressIndex][0] * MotionSettings.getEncoderTicksPerMetre())) progressIndex++;
 		
 		double[] curvatures = new double[Math.min(MotionSettings.trajectoryAngleForesight, waypoints.length - progressIndex)];
-		
 		for(int i = 0; i < curvatures.length; i++) curvatures[i] = waypoints[i + progressIndex][2];
 		
 		return avg(curvatures);
